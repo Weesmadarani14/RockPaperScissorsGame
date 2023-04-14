4fc2eab89d2aafd15897f193184b166c527df521
Wissam Madarani <wissammadarani@gmail.com>
2023-04-14 16:04:15 -0400

    script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const winningChoices = ["rock", "paper", "scissors"];
 let playerWins = 0;
 let computerWins = 0;
 
+
 // define variables for results, playerScore, and computerScore
 const resultElement = document.getElementById("result");
 const playerScoreElement = document.getElementById("playerScore");
@@ -18,51 +19,44 @@ function disableButtons () {
 // play function 
 function play (playerSelection) {
 
-        // computer choice
-        const computerSelection = winningChoices[Math.floor(Math.random() * winningChoices.length)];
-        let result;
+    // computer choice
+    const computerSelection = winningChoices[Math.floor(Math.random() * winningChoices.length)];
+    let result;
         
-        // result of game 
-    if (
-        (playerSelection === computerSelection) 
-        )
-    {
+    // result of game 
+if ((playerSelection === computerSelection)){
         result = "It's a tie";
-    }
-        
-    else if (
-        (playerSelection === "rock" && computerSelection === "scissors") ||
-        (playerSelection === "paper" && computerSelection === "rock")  ||
-        (playerSelection === "scissors" && computerSelection === "paper")
-    ) 
-    {
-        result = "You win!!!!";
-        playerWins++;
-    }
-    else {
-        result = "Computer wins :(";
-        computerWins++;
-    }
+} else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock")  ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+) {
+    result = "You win!!!!";
+    playerWins++;
+} else {
+    result = "Computer wins :(";
+    computerWins++;
+}
 
     // display result of game in result element
     resultElement.textContent = `You chose ${playerSelection}. Computer chose ${computerSelection}. ${result}`;
 
+     // display scoreboard
+     document.getElementById("playerScore").textContent = `Player: ${playerWins}`;
+     document.getElementById("computerScore").textContent = `Computer: ${computerWins}`;
 
     // scoreboard process
     if (playerWins === 5) {
-        resultElement.textContent = "Congratulations!! You win the game!!! Refresh page to play again.";
+        resultElement.textContent = 
+        "Congratulations!! You win the game!!! Refresh page to play again.";
+        disableButtons();
+        return;
+    } else if (computerWins === 5) {
+        resultElement.textContent = 
+        "Computer wins! Try again next time! Refresh page to play again.";
         disableButtons();
         return;
     }
-    else if (computerWins === 5) {
-        resultElement.textContent = "Computer wins! Try again next time! Refresh page to play again."
-        disableButtons();
-        return;
-    }
-
-    // display scoreboard
-    document.getElementById("playerScore").textContent = `Player: ${playerWins}`;
-    document.getElementById("computerScore").textContent = `Computer: ${computerWins}`;
 
     // result of who won the round
     document.getElementById("result").textContent = `You chose ${playerSelection}. Computer chose ${computerSelection}. ${result}`;
@@ -83,6 +77,3 @@ function play (playerSelection) {
         play("scissors");
       });
  
-      
-
-      
